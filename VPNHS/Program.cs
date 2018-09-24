bbc93d4b05ef6fc8a381bda4faa4a04e3535f2d9
ps <peta.stadnik@hotmail.cz>
2018-09-24 14:01:02 +0200

    Přidejte soubory projektu.

---
+++ VPNHS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VPNHS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
